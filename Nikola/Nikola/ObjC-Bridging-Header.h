17e50822d811411b3eb8f15aebae0615f189f1af
William Clark <william.a.clark@gmail.com>
2014-08-01 10:55:10 -0700

    adding RSColorPicker + necessary support files
    Including bridging header and prefix header

---
+++ Nikola/Nikola/ObjC-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  ObjC-Bridging-Header.h
+//  Nikola
+//
+//  Created by William Clark on 8/1/14.
+//  Copyright (c) 2014 fuseproject. All rights reserved.
+//
+
+#import "ColorPickerClasses/RSColorPickerView.h"
+#import "ColorPickerClasses/RSColorFunctions.h"
